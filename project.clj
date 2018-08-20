098982bb8eadc4763938c6429b154cdd68b688dc
asotnikov <asotnikov.100@gmail.com>
2018-08-20 14:40:57 +0700

    Configuring CI

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject buran "0.1.0"
+
   :description "Bidirectional RSS/Atom feed consumer(parser) and producer(writer)"
+
   :url "https://github.com/alekseysotnikov/buran"
+
   :license {:name "Eclipse Public License 2.0"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.rometools/rome "1.11.0"]])
+                 [com.rometools/rome "1.11.0"]]
+
+  :plugins [[lein-cloverage "1.0.13"]])
